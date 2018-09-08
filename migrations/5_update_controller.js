90cfe2e3bbfaa9c8577900012e2a55e76837806b
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-09-08 18:35:00 +0200

    ControllerPointer tests done

---
+++ migrations/5_update_controller.js
@@ -0,0 +1,21 @@
+var Controller = artifacts.require('./Controller.sol')
+var ControllerPointer = artifacts.require('./ControllerPointer.sol')
+let _ = '        '
+
+module.exports = (deployer, helper, accounts) => {
+  deployer.then(async () => {
+    try {
+      await deployer.deploy(Controller, { overwrite: true })
+      const controller = await Controller.deployed()
+      console.log(_ + 'Controller deployed at: ' + controller.address)
+
+      const controllerPointer = await ControllerPointer.deployed()
+      console.log(
+        _ + 'ControllerPointer deployed at: ' + controllerPointer.address
+      )
+      var tx = await controllerPointer.setController(controller.address)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+}
--- test/ControllerPointer.test.js
+++ test/ControllerPointer.test.js
@@ -1,40 +1,97 @@
 var utils = require('web3-utils')
 var ControllerPointer = artifacts.require('./ControllerPointer.sol')
+var Controller = artifacts.require('./Controller.sol')
 
-let gasPrice = 1000000000 // 1GWEI
+let ONEGWEI = 1000000000 // 1GWEI
 
 let _ = '        '
 
 contract('ControllerPointer', async function(accounts) {
-  let controllerPointer
+  let controllerPointer, controller
 
-  before(done => {
-    ;(async () => {
+  describe('ControllerPointer.sol', function() {
+    before(done => {
+      ;(async () => {
+        try {
+          var totalGas = new web3.BigNumber(0)
+
+          // Deploy Controller.sol
+          controller = await Controller.new()
+          var tx = web3.eth.getTransactionReceipt(controller.transactionHash)
+          totalGas = totalGas.plus(tx.gasUsed)
+          console.log(_ + tx.gasUsed + ' - Deploy controller')
+
+          // Deploy ControllerPointer.sol
+          controllerPointer = await ControllerPointer.new(controller.address)
+          var tx = web3.eth.getTransactionReceipt(
+            controllerPointer.transactionHash
+          )
+          totalGas = totalGas.plus(tx.gasUsed)
+          console.log(_ + tx.gasUsed + ' - Deploy controllerPointer')
+
+          console.log(_ + '-----------------------')
+          console.log(_ + totalGas.toFormat(0) + ' - Total Gas')
+          done()
+        } catch (error) {
+          console.error(error)
+          done(false)
+        }
+      })()
+    })
+    it('getController should return controller', async function() {
+      let controllerAddress = await controllerPointer.getController()
+      assert(
+        controllerAddress === controller.address,
+        'ControllerAddress (' +
+          controllerAddress +
+          ') did not equal controller.address (' +
+          controller.address +
+          ')'
+      )
+    })
+    it('setController should set & return controller', async function() {
+      let controllerAddress = accounts[1]
+      await controllerPointer.setController(controllerAddress)
+      const returnedControllerAddress = await controllerPointer.getController()
+      assert(
+        controllerAddress === returnedControllerAddress,
+        'ControllerAddress (' +
+          controllerAddress +
+          ') did not equal returnedControllerAddress (' +
+          returnedControllerAddress +
+          ')'
+      )
+    })
+    it('setERC20Main should set & return ERC20Main', async function() {
+      let erc20Set = await controllerPointer.erc20Set()
+      assert(!erc20Set, 'erc20Set should be false but it is ' + erc20Set)
+
+      let ERC20Address = accounts[2]
+      await controllerPointer.setERC20Main(ERC20Address)
+      const returnedERC20Address = await controllerPointer.getERC20Main()
+      assert(
+        ERC20Address === returnedERC20Address,
+        'ERC20Address (' +
+          ERC20Address +
+          ') did not equal returnedERC20Address (' +
+          returnedERC20Address +
+          ')'
+      )
+    })
+    // this one is skipped until we turn this feature back on
+    it.skip('setERC20Main should fail the second time', async function() {
+      let ERC20Address = accounts[3]
+      let tx
       try {
-        var totalGas = new web3.BigNumber(0)
-
-        // Deploy ControllerPointer.sol
-        controllerPointer = await ControllerPointer.new()
-        var tx = web3.eth.getTransactionReceipt(
-          controllerPointer.transactionHash
-        )
-        totalGas = totalGas.plus(tx.gasUsed)
-        console.log(_ + tx.gasUsed + ' - Deploy controllerPointer')
-        controllerPointer = await ControllerPointer.deployed()
-
-        console.log(_ + '-----------------------')
-        console.log(_ + totalGas.toFormat(0) + ' - Total Gas')
-        done()
+        tx = await controllerPointer.setERC20Main(ERC20Address)
       } catch (error) {
-        console.error(error)
-        done(false)
+        assert(true)
+        return
       }
-    })()
-  })
-
-  describe('Sample.sol', function() {
-    it('should pass', async function() {
-      assert(true === true, 'this is true')
+      assert(
+        tx.receipt.status === '0x00',
+        'setERC20 a second time should have failed'
+      )
     })
   })
 })
