fd39e57737c8851627732493933895418de6c4a7
anon <sarah.anne.friend@gmail.com>
2018-09-08 10:51:58 -0400

    Set erc20main on controllerpointer

---
+++ migrations/4_setERC20Main.js
@@ -0,0 +1,16 @@
+var Erc20Main = artifacts.require('./ERC20Main.sol')
+var ControllerPointer = artifacts.require('./ControllerPointer.sol')
+let _ = '        '
+
+module.exports = (deployer, helper, accounts) => {
+  deployer.then(async () => {
+    try {
+      const erc20Main = await Erc20Main.deployed()
+      const controllerPointer = await ControllerPointer.deployed()
+      await controllerPointer.setERC20Main(erc20Main.address)
+      console.log(_ + 'Set ControllerPointer to have ERC20Main: ' + erc20Main.address)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+}
