eb8aca032f7c18d1c5602c401101e1d1858d8fff
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-09-09 02:06:36 +0200

    output contracts

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var ERC20Main = require('./build/contracts/ERC20Main.json')
-var Controller = require('./build/contracts/Controller.json')
+var ControllerPointer = require('./abi.json').ControllerPointer
+var Controller = require('./abi.json').Controller
+var ERC20Main = require('./abi.json').ERC20Main
+var ProxyFactory = require('./abi.json').ProxyFactory
 
 module.exports = {
+  ControllerPointer,
+  Controller,
   ERC20Main,
-  Controller
+  ProxyFactory
 }
